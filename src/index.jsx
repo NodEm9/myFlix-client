4deaf55f0d7c647eb3091e377f1df70922e2f059
Emmanuel Nodolomwanyi <emma0martins@gmail.com>
2024-06-06 20:44:14 +0200

    Import main view

---
+++ src/index.jsx
@@ -0,0 +1,18 @@
+import { createRoot } from "react-dom/client";
+import { MainView } from "./components/main-view/main-view";
+
+import "./index.scss";
+
+
+const App = () => { 
+  return (
+    <div className="my-flix">
+      <MainView />
+    </div>
+  );
+};
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+
+root.render(<App />);
