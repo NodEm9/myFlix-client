db81862ebc8602deb95717cb899bb4a45eacc201
Emmanuel Nodolomwanyi <emma0martins@gmail.com>
2024-06-07 15:13:14 +0200

    Add keys to each element to

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,44 +1,71 @@
 export const MovieView = ({ movie, onBackClick }) => {
   return (
     <div className="movie-view">
-      <div>
-        <img src={movie.ImageURL} />
+      <div >
+        <img src={movie.ImageUrl} key={movie.ImageUrl} />
       </div>
       <div>
         <span>Title:</span>
-        <span className="movie-title">{movie.Title}</span>
+        <span className="movie-title" key={movie.Title}>{movie.Title}</span>
       </div>
       <div>
         <span>Description: </span>
-        <div className="movie-description">{movie.Description}</div>
+        <div className="movie-description" key={movie.Description}>
+          {movie.Description}
+        </div>
       </div>
       <div>
         <span>Genre: </span>
-        <div className="movie-genre">{movie.Genre}</div>
+        <span className="movie-genre">
+          {movie.Genre.map(genre => (
+            <div key={movie._id}>
+              <span>{genre.name}</span>
+              <p>{genre.description}</p>
+            </div>
+          ))}</span>
       </div>
       <div>
         <span>Director: </span>
-        <div className="movie-director">{movie.Director}</div>
+        <div className="movie-director">
+          {movie.Director.map(director => (
+            <div key={director.name}>
+              <span>{director.name}</span>
+              <span>{director.bio}</span>
+              <span>{director.birthyear}</span>
+              <span>{director.deathyear}</span>
+            </div>
+
+          ))}</div>
       </div>
       <div>
         <span>Actors: </span>
-        <div className="movie-actors">{movie.Actors}</div>
-      </div>
-      <div>
-        <span>Year: </span>
-        <div className="movie-year">{movie.Year}</div>
-      </div>
-      <div>
-        <span>Rating: </span>
-        <div className="movie-rating">{movie.Rating}</div>
+        <div className="movie-actors">
+          {movie.Actor.map(actor => (
+            <div key={movie._id}>
+              <span>{actor.name}</span>
+              <span>{actor.birthyear}</span>
+              <span>{actor.bio}</span>
+            </div>
+
+          ))}</div>
       </div>
       <div>
         <span>ReleaseDate: </span>
-        <div className="movie-runtime">{movie.ReleaseDate}</div>
+        <div className="movie-runtime" key={movie.ReleaseDate}>
+          {movie.ReleaseDate}
+        </div>
+      </div>
+      <div>
+        <span>Rating: </span>
+        <div className="movie-rating" key={movie.Rating}>
+          {movie.Rating}
+        </div>
       </div>
       <div>
         <span>Featured: </span>
-        <div className="movie-runtime">{movie.Featured}</div>
+        <div className="movie-runtime" key={movie.Featured}>
+          {movie.Featured}
+        </div>
       </div>
       <button onClick={onBackClick}>Back</button>
     </div>
