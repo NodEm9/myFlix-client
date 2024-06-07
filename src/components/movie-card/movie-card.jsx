ccf51e5ad61b773df4063f2ace4fae9e89bc4799
Emmanuel Nodolomwanyi <emma0martins@gmail.com>
2024-06-07 15:12:02 +0200

    Add PropType to validate props

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,3 +1,6 @@
+import PropTypes from 'prop-types';
+
+
 export const MovieCard = ({ movie, onMovieClick }) => {
   return (  
     <div
@@ -10,3 +13,18 @@ export const MovieCard = ({ movie, onMovieClick }) => {
     </div>   
   );  
 }; 
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    Title: PropTypes.string.isRequired,
+    Description: PropTypes.string.isRequired,
+    Genre: PropTypes.arrayOf(PropTypes.object),
+    Director: PropTypes.arrayOf(PropTypes.object),
+    Actor: PropTypes.arrayOf(PropTypes.object),
+    ReleaseDate: PropTypes.string.isRequired,
+    ImageURL: PropTypes.string,
+    Rating: PropTypes.number,
+    Featured: PropTypes.bool
+  }).isRequired,
+  onMovieClick: PropTypes.func.isRequired
+};
