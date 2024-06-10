ce00f4e8c8dee93ef971d5b9c079f6fb3aea7e5e
Emmanuel Nodolomwanyi <emma0martins@gmail.com>
2024-06-10 15:27:52 +0200

    Add navbar to page index

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,14 @@
 import { createRoot } from "react-dom/client";
 import { MainView } from "./components/main-view/main-view";
+import { Navbar } from "./components/navigation/navbar";
 
 import "./index.scss";
 
 
 const App = () => { 
   return (
-    <div className="my-flix">
+    <div className="App">
+      <Navbar />
       <MainView />
     </div>
   );
