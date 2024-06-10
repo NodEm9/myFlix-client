fb98007f96a3d54129f5abe9887385c8fb1a628e
Emmanuel Nodolomwanyi <emma0martins@gmail.com>
2024-06-10 15:26:37 +0200

    Implement  loading skeleton  for better user expireince

---
+++ src/components/loading/skeleton.jsx
@@ -0,0 +1,12 @@
+const  Skeleton = () => {
+  return (
+    <div className="movie-card">
+      <div className="movie-card__header"></div>
+      <div className="movie-card__title"></div>
+      <div className="movie-card__description"></div>
+      <div className="movie-card__genre"></div>
+    </div>
+  )
+}
+
+export default Skeleton
