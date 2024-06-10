8abaedc2803de7180b5e08285603cc74fad260cb
Emmanuel Nodolomwanyi <emma0martins@gmail.com>
2024-06-10 15:23:47 +0200

    Implement PropTypes to check and make sure intended props are pass correctly to the component

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,15 +1,20 @@
 import PropTypes from 'prop-types';
-
+import { CustomImage } from '../custom-image/custom-image';
 
 export const MovieCard = ({ movie, onMovieClick }) => {
   return (  
     <div
-      className="movie-card"
+      className="movie-card" 
       onClick={() => {
         onMovieClick(movie);
       }}
     >
-      {movie.Title}
+      <div className='movie-card__header'>
+        <CustomImage src={movie.ImageUrl} alt={"Movie Poster"} />
+      </div>
+      <h3 className='movie-card__title'>{movie.Title}</h3>
+      <p className='movie-card__description'>{movie.Description}</p>
+      <p className='movie-card__genre'>Genre</p>
     </div>   
   );  
 }; 
@@ -20,10 +25,10 @@ MovieCard.propTypes = {
     Description: PropTypes.string.isRequired,
     Genre: PropTypes.arrayOf(PropTypes.object),
     Director: PropTypes.arrayOf(PropTypes.object),
-    Actor: PropTypes.arrayOf(PropTypes.object),
+    Actor: PropTypes.arrayOf(PropTypes.string),
     ReleaseDate: PropTypes.string.isRequired,
     ImageURL: PropTypes.string,
-    Rating: PropTypes.number,
+    Rating: PropTypes.number, 
     Featured: PropTypes.bool
   }).isRequired,
   onMovieClick: PropTypes.func.isRequired
