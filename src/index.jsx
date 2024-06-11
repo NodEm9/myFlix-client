cc34008c96c7d7ad90d7de20475fd06c6a5cd7a9
Emmanuel Nodolomwanyi <emma0martins@gmail.com>
2024-06-11 19:00:14 +0200

    remove unse import

--- src/index.jsx
+++ src/index.jsx
@@ -2,14 +2,16 @@ import { createRoot } from "react-dom/client";
 import { MainView } from "./components/main-view/main-view";
 import { Navbar } from "./components/navigation/navbar";
 
+
 import "./index.scss";
+
 
 
 const App = () => { 
   return (
     <div className="App">
-      <Navbar />
-      <MainView />
+        <Navbar />
+        <MainView />
     </div>
   );
 };
