f3590ff88fdb663ecfaaf2a5145b3ad26c6527ad
Emmanuel Nodolomwanyi <emma0martins@gmail.com>
2024-06-13 22:05:10 +0200

    Add bootstrap components to restyle cards

--- src/components/loading/skeleton.jsx
+++ src/components/loading/skeleton.jsx
@@ -1,12 +1,14 @@
+import Card from "react-bootstrap/Card"
+
 const  Skeleton = () => {
   return (
-    <div className="movie-card">
+    <div  className="movie-card">
       <div className="movie-card__header"></div>
       <div className="movie-card__title"></div>
       <div className="movie-card__description"></div>
       <div className="movie-card__genre"></div>
     </div>
-  )
+  ) 
 }
 
 export default Skeleton
