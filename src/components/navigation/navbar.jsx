f181627799dcd0db3f08dd86defe93bebfbeddf7
Emmanuel Nodolomwanyi <emma0martins@gmail.com>
2024-06-15 00:00:04 +0200

    Add border bottom and postition fixed

--- src/components/navigation/navbar.jsx
+++ src/components/navigation/navbar.jsx
@@ -2,18 +2,18 @@ import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
 import { useState } from 'react';
 
-export const Navbar = () => {
+export const Navigationbar = () => {
  let user = JSON.parse(localStorage.getItem("user"));
   const [loggedInUser, setLoggedInUser] = useState(user ? user.Username : "Guest");
  
   return (
-    <Navbar className="bg-body p-0">
-      <Container className='bg-body p-4'>
-        <Navbar.Brand href="#home" className="fw-bold fs-2">myFlix</Navbar.Brand>
+    <Navbar className="bg-body p-0 mb-5 ">
+      <Container className='bg-body p-4 fixed-top border-bottom'> 
+        <Navbar.Brand href="/" className="fw-bold fs-2">myFlix</Navbar.Brand>
         <Navbar.Toggle />
         <Navbar.Collapse className="justify-content-end">
           <Navbar.Text>
-            Signed in as: <a href="#login">{loggedInUser}</a>
+            Signed in as: <a href="/login">{loggedInUser}</a>
           </Navbar.Text>
         </Navbar.Collapse>
       </Container>
