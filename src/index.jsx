897e86222e394b256c060df53caebdc724772e0c
Emmanuel Nodolomwanyi <emma0martins@gmail.com>
2024-06-15 00:01:16 +0200

    Implement toast

--- src/index.jsx
+++ src/index.jsx
@@ -1,15 +1,17 @@
 import { createRoot } from "react-dom/client";
 import { MainView } from "./components/main-view/main-view";
-import { Navbar } from "./components/navigation/navbar";
-
+import { Navigationbar } from "./components/navigation/navbar";
 import Container from "react-bootstrap/Container";
 
 import "./scss/index.scss";
+import Row from "react-bootstrap/Row";
 
 const App = () => {
-  return ( 
-    <Container className="App" > 
-      <Navbar />
+  return (
+    <Container className="App h-100" >
+      <Row>
+        <Navigationbar />
+      </Row>
       <MainView />
     </Container>
   );
