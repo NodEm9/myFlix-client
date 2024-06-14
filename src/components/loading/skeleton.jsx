f399a6acba296592986c2a6f0100b8231bac20ec
Emmanuel Nodolomwanyi <emma0martins@gmail.com>
2024-06-14 23:41:23 +0200

    Add bootstrap Component

--- src/components/loading/skeleton.jsx
+++ src/components/loading/skeleton.jsx
@@ -1,14 +1,24 @@
+import Row from "react-bootstrap/Row"
 import Card from "react-bootstrap/Card"
+import { Col } from "react-bootstrap" 
 
-const  Skeleton = () => {
+const Skeleton = () => { 
   return (
-    <div  className="movie-card">
-      <div className="movie-card__header"></div>
-      <div className="movie-card__title"></div>
-      <div className="movie-card__description"></div>
-      <div className="movie-card__genre"></div>
-    </div>
-  ) 
+    <Row md={8} className="mt-5 justify-content-md-center">
+      <Col md={5} className="hstack gap-3 justify-content-md-center">
+      <Card className="movie-card p-5">
+        <Card.Body>
+          <Card.Title className="skeleton-title"></Card.Title>
+          <Card.Text className="skeleton-text"></Card.Text>
+          <Card.Text className="skeleton-text"></Card.Text>
+          <Card.Text className="skeleton-text"></Card.Text>
+          <Card.Text className="skeleton-text"></Card.Text>
+        </Card.Body>
+        <Card.Img variant="top" className="skeleton-image" />
+      </Card>
+      </Col>
+    </Row>
+  )
 }
 
 export default Skeleton
