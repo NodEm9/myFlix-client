a975e98732fe46a58fa71632b56ec85e9910137c
Emmanuel Nodolomwanyi <emma0martins@gmail.com>
2024-06-17 20:56:25 +0200

    Restyle skeleton card

--- src/components/loading/skeleton.jsx
+++ src/components/loading/skeleton.jsx
@@ -7,17 +7,17 @@ const Skeleton = () => {
   let loadingCard = new Array(8).fill(0)
 
   return (
-    <Row md={12} className="mt-5 justify-content-md-center">
+    <Row md={12} className="mt-5 justify-content-md-center">   
+      <Col md={5}  className="skeleton vstack gap-3">
       {loadingCard.map((index) => (
-        <Col md={5} key={index}  className="skeleton vstack gap-3">
-            <Card className="skeleton-card">
+            <Card key={index} className="skeleton-card">
               <Card.Body>
                 <Card.Title className="skeleton-title"></Card.Title>
                 <Card.Text className="skeleton-text"></Card.Text>
               </Card.Body>
-            </Card>
-        </Col>
+        </Card>
       ))}
+        </Col>
     </Row>
   )
 }
