d7b7cc5ed87bf8e4fcc6dc5e49e11d6de228fceb
Emmanuel Nodolomwanyi <emma0martins@gmail.com>
2024-06-20 19:02:41 +0200

    Reduce footer  height

--- src/components/footer-view/footer-view.jsx
+++ src/components/footer-view/footer-view.jsx
@@ -5,18 +5,21 @@ import Nav from "react-bootstrap/Nav";
 
 export const FooterView = () => {
   return (
-    <Row className="bg-dark text-white text-center p-3 pb-5 mt-5 d-md-flex d-sm-flex">
+    <Row className="bg-dark text-white text-center p-3 d-md-flex d-sm-flex">
       <Col className="p-5">
          &copy; 2024 myFlix. All Rights Reserved.
       </Col>
       <Col >
-        <Nav className="pb-5 d-md-flex d-sm-flex flex-column gap-3">
+        <Nav className="p-2 d-md-flex d-sm-flex flex-column gap-3">
           <Nav.Link href="/" className="text-primary fs-2 text-decoration-none">
             myFlix</Nav.Link>
           <Nav.Link href="/movies" className="text-white fs-6 text-decoration-none">
             Movies</Nav.Link>
           <Nav.Link href="/users" className="text-white fs-6 text-decoration-none">
             Users</Nav.Link>
+          <Nav.Link href="/" className="text-white fs-6 text-decoration-none">
+            About Us</Nav.Link>
+          
         </Nav>
       </Col>
     </Row>
