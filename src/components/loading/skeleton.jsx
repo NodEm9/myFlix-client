5f43ce279b58efcb20c6f22ebf8a1679685233bc
Emmanuel Nodolomwanyi <emma0martins@gmail.com>
2024-06-25 12:58:42 +0200

    add grid to skeleton

--- src/components/loading/skeleton.jsx
+++ src/components/loading/skeleton.jsx
@@ -2,15 +2,18 @@ import { Col, Row } from "react-bootstrap"
 import Card from "react-bootstrap/Card"
 
 const Skeleton = () => {
+
   return (
     <Row md={12} className="card-container">
-      <Col md={8} >
-        <Card className="skeleton">
-          <div className="skeleton__header"></div>
-          <div className="skeleton__title"></div>
-          <div className="skeleton__description"></div>
-          <div className="skeleton__genre"></div>
-        </Card> 
+      <Col md={8} className="p-5 gap-3" >
+        {Array.from({ length: 12 }).fill(0).map((_, index) => (
+          <Card key={index} className="skeleton">
+            <div className="skeleton__header"></div>
+            <div className="skeleton__title"></div>
+            <div className="skeleton__description"></div>
+            <div className="skeleton__genre"></div>
+          </Card>
+        ))}
       </Col>
     </Row>
   )
