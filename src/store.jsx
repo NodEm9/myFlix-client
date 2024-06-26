ba81aa8915077a3055feadf62be139c8cde7d6df
Emmanuel Nodolomwanyi <emma0martins@gmail.com>
2024-06-26 18:54:34 +0200

    Add user reducer to store

--- src/store.jsx
+++ src/store.jsx
@@ -1,5 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
+import movieReducer from "./redux/movies/movieSlice"
+import userReducer from "./redux/user/userSlice"
 
 export default configureStore({
-  reducer: {},
+  reducer: {
+    movies: movieReducer,
+    user: userReducer
+  },
 })
