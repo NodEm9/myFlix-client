15d08c191716fe731ccbe0e5f5da4aac87fc655e
Emmanuel Nodolomwanyi <emma0martins@gmail.com>
2024-06-26 18:56:57 +0200

    Add skeleton for loading

--- src/components/loading/skeleton.jsx
+++ src/components/loading/skeleton.jsx
@@ -6,20 +6,20 @@ import "./loading-skeleton.scss"
 
 
 const Skeleton = () => {
-
   return (
-    <Row md={12} className="card-container">
-      <Col md={8} className="p-5 gap-3" >
-        {Array.from({ length: 12 }).fill(0).map((_, index) => (
-          <Card key={index} className="skeleton">
+    <Row  className="justify-content-center">
+      {Array.from({ length: 12 }).fill(0).map((_, index) => (
+        <Col md={3} key={index} className="d-flex m-3  justify-content-center gap-5" >
+          <Card className="skeleton">
             <div className="skeleton__header"></div>
             <div className="skeleton__title"></div>
             <div className="skeleton__description"></div>
             <div className="skeleton__genre"></div>
-          </Card>
-        ))}
+          </Card> 
       </Col>
+      ))}
+
     </Row>
   )
-  }
+}
 export default Skeleton
