46dbfe7e01c1aad504cca53e8b0a8c8271209c6d
Emmanuel Nodolomwanyi <emma0martins@gmail.com>
2024-06-26 19:15:58 +0200

    Add user reducer

---
+++ src/redux/user/userSlice.jsx
@@ -0,0 +1,35 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+
+const userSlice = createSlice({
+  name: "user",
+  initialState: {
+    user: JSON.parse(localStorage.getItem("user")),
+    token: localStorage.getItem("token"),
+    deleteUser: null,
+    loggedIn: Boolean(localStorage.getItem("token"))
+  },
+  reducers: {
+    setUser: (state, action) => {
+      state.user = action.payload;
+    },
+    setToken: (state, action) => {
+      state.token = action.payload;
+    },
+    setDeleteUser: (state, action) => {
+      state.deleteUser = action.payload;
+    },
+
+    setLoggedIn: (state, action) => {
+      state.loggedIn = action.payload;
+    }
+  },
+});
+
+export const {
+  setUser,
+  setToken,
+  setDeleteUser,
+  setLoggedIn } = userSlice.actions;
+
+export default userSlice.reducer;
