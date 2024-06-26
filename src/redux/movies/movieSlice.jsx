4cb368c51f0cf2278ab9c1b416f20395e8d96eaf
Emmanuel Nodolomwanyi <emma0martins@gmail.com>
2024-06-26 19:13:33 +0200

    Add movies reducer

---
+++ src/redux/movies/movieSlice.jsx
@@ -0,0 +1,21 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const movieSlice = createSlice({
+  name: "movies",
+  initialState: {
+    movies: [],
+    filter: "",
+  },
+  reducers: {
+    setMovies: (state, action) => {
+      state.movies = action.payload;
+    },
+    setFilter: (state, action) => {
+      state.filter = action.payload;
+    },
+  },
+});
+
+export const { setMovies, setFilter } = movieSlice.actions;
+
+export default movieSlice.reducer;
