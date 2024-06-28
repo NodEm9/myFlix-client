32a9cbc100f398ab4c86e3783d0a8519d5208f43
Emmanuel Nodolomwanyi <emma0martins@gmail.com>
2024-06-28 18:44:53 +0200

    Remove unnecessary code

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -2,7 +2,6 @@ import { useState } from "react";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import Col from "react-bootstrap/Col";
-import "./login-view.scss";
 import ToastNotification from "../toast/toast";
 import { setUser, setLoggedIn, setToken } from "../../redux/user/userSlice";
 import { useDispatch } from "react-redux";
