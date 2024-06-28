87e2cbd702e3dbf5227cfaff21042301777618eb
Emmanuel Nodolomwanyi <emma0martins@gmail.com>
2024-06-28 18:49:26 +0200

    Remove react link import

--- src/components/footer-view/footer-view.jsx
+++ src/components/footer-view/footer-view.jsx
@@ -1,16 +1,14 @@
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import { Link } from "react-router-dom";
 import Nav from "react-bootstrap/Nav";
 
 export const FooterView = () => {
   return (
-    <Row className="bg-dark text-white text-center p-3 d-md-flex d-sm-flex">
-      <Col className="p-5">
+    <Row className="footer bg-dark text-white text-center">
+      <Col className="footer__copyright">
          &copy; 2024 myFlix. All Rights Reserved.
       </Col>
-      <Col >
-      
+      <Col >   
         <Nav className="p-2 d-md-flex d-sm-flex flex-column gap-3">
           <Nav.Link href="/" className="text-primary fs-2 text-decoration-none">
             myFlix</Nav.Link>
@@ -20,7 +18,6 @@ export const FooterView = () => {
             Users</Nav.Link>
           <Nav.Link href={"https://myflix-app-led6.onrender.com/documentation.html"} target="_blank" className="text-white fs-6 text-decoration-none">
            API Documentation</Nav.Link>
-          
         </Nav>
       </Col>
     </Row>
