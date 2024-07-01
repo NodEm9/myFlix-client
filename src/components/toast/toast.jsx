ea50e93a962b016a31c74afeaa0742b23b3e3722
Emmanuel Nodolomwanyi <emma0martins@gmail.com>
2024-07-01 10:46:55 +0200

    Bundle files with parcel

--- src/components/toast/toast.jsx
+++ src/components/toast/toast.jsx
@@ -4,37 +4,37 @@ import Toast from 'react-bootstrap/Toast';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
-const ToastNotification = ({ message, txtColor }) => {
+ const ToastNotification = ({ message, txtColor}) => {
   const [show, setShow] = useState(true);
   const [tick, setTick] = useState(0);
 
   const toggleShow = () => setShow(!show);
 
   // Create a function to keep track of time
-  const getTimer = (time) => {
-    let date = new Date();
+  const getTimer =  (time) => {
+    let date = new Date(); 
     date.getTime(time).toLocaleString();
     setInterval(() => {
       if (show) {
         setTick(time + 1);
-      }
+      } 
     }, 6000)
     return time;
   };
 
-  // Create a variable to hold the current time 
-  //and call the getTime function
+    // Create a variable to hold the current time 
+  //and call the timeKeeper function
   let time = getTimer(tick);
 
 
   return (
-    <Row md={8} className='mt-md-5 pt-md-5'>
+      <Row md={8} className='mt-md-5 pt-md-5'>
       <Col md={6} className="mb-2">
         <ToastContainer
           position="top-end"
           className="p-3"
         >
-          <Toast show={show} onClose={toggleShow} delay={5000} autohide>
+          <Toast show={show} onClose={toggleShow} delay={50000} autohide>
             <Toast.Header>
               <strong className="me-auto">System notification</strong>
               <small>{`${time} seconds ago`}</small>
@@ -42,8 +42,8 @@ const ToastNotification = ({ message, txtColor }) => {
             <Toast.Body className={`${txtColor}`} >{message}</Toast.Body>
           </Toast>
         </ToastContainer>
-      </Col>
-    </Row>
+          </Col>
+        </Row>
   );
 };
 
