75f8cad0b87b06ad7900d93bcc520349c8931b58
Emmanuel Nodolomwanyi <emma0martins@gmail.com>
2024-07-01 10:36:27 +0200

    Create redux user slice to mavie user state

---
+++ src/redux/user/userSlice.js
@@ -0,0 +1,40 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+
+const userSlice = createSlice({
+  name: "user",
+  initialState: {
+    user: JSON.parse(localStorage.getItem("user")),
+    token: localStorage.getItem("token"),
+    favoriteMovies: [],
+    deleteUser: null,
+    loggedIn: Boolean(localStorage.getItem("token")),
+  },
+  reducers: {
+    setUser: (state, action) => {
+      state.user = action.payload;
+    },
+    setToken: (state, action) => {
+      state.token = action.payload;
+    },
+    setDeleteUser: (state, action) => {
+      state.deleteUser = action.payload;
+    },
+    setFavoriteMovies: (state, action) => {
+      state.favoriteMovies = state.favoriteMovies.push(action.payload);
+    },
+    setLoggedIn: (state, action) => {
+      state.loggedIn = action.payload;
+    },
+  },
+});
+
+export const {
+  setUser,
+  setToken,
+  setFavoriteMovies,
+  setDeleteUser,
+  setLoggedIn,
+} = userSlice.actions;
+
+export default userSlice.reducer;
