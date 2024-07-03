e7cf2dfbcf3ecb0f084f7b32df6c64bd3d8486c3
Emmanuel Nodolomwanyi <emma0martins@gmail.com>
2024-07-03 14:05:51 +0200

    Remove import useSelector

--- src/components/profile-view/profile-view.jsx
+++ src/components/profile-view/profile-view.jsx
@@ -1,42 +1,43 @@
+import React from 'react';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import { UpdateUserData } from './updateUser';
 import { DeleteUser } from './delete-user';
 import { UserFavoriteMovies } from './favoriteMovie';
-import { useSelector } from 'react-redux';
+import PropTypes from 'prop-types'
 import './profile-view.scss';
 
 
-export const ProfileView = () => {
-  const movies = useSelector((state) => state.movies.movies);
-  const user = useSelector((state) => state.user.user);
-  const favoriteMovies = movies.filter((movie) =>  user.favoriteMovies.includes(movie._id)); 
-    
+
+export const ProfileView = ({ movies }) => {
+  const user = JSON.parse(localStorage.getItem('user'));
+  const favoriteMovies = movies.filter((movie) => user.favoriteMovies.includes(movie._id)); 
+
 
   return (
     <Row className="h-100">
       <Row className='profile-view'>
         <Col className='profile fs-5'>
-        <h2 className='fw-bold'>Account Information</h2>
-        <div className="username">
-          <span className="label fw-semibold">Username: </span>
-          <span className="value">{user.Username}</span>
-        </div>
-        <div className="email">
-          <span className="label fw-semibold">Email: </span>
-          <span className="value">{user.Email}</span>
-        </div>
-        <div className="birthday">
-          <span className="label fw-semibold">Birthday: </span>
-          <span className="value">{user.Birthday.slice(0, 10)}</span>
-        </div>
-        <div>
-          <span className="label fw-semibold">Role: </span>
-          <span className='value'>{user.Role}</span>
-        </div>
-      </Col>
-      <Col className='my-4'>
-        <UpdateUserData user={user} />
+          <h2 className='fw-bold'>Account Information</h2>
+          <div className="username">
+            <span className="label fw-semibold">Username: </span>
+            <span className="value">{user.Username}</span>
+          </div>
+          <div className="email">
+            <span className="label fw-semibold">Email: </span>
+            <span className="value">{user.Email}</span>
+          </div>
+          <div className="birthday">
+            <span className="label fw-semibold">Birthday: </span>
+            <span className="value">{user.Birthday.slice(0, 10)}</span>
+          </div>
+          <div>
+            <span className="label fw-semibold">Role: </span>
+            <span className='value'>{user.Role}</span>
+          </div>
+        </Col>
+        <Col className='my-4'>
+          <UpdateUserData user={user} />
         </Col>
       </Row>
       <Row className='d-flex justify-content-center'>
@@ -48,9 +49,13 @@ export const ProfileView = () => {
       <Row className='d-flex justify-content-center'>
         <h2 className='fw-bold'>Favorite Movies</h2>
         <Col className='d-flex justify-content-center'>
-            <UserFavoriteMovies favoriteMovies={favoriteMovies} />
+          {favoriteMovies && <UserFavoriteMovies user={user} favoriteMovies={favoriteMovies} />}
         </Col>
       </Row>
     </Row>
   );
+};
+
+ProfileView.propTypes = {
+  movies: PropTypes.array.isRequired,
 };
