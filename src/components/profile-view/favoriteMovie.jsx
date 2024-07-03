037c8e4dda68e1e21e7508c2daae3e671a02b6cc
Emmanuel Nodolomwanyi <emma0martins@gmail.com>
2024-07-03 14:08:55 +0200

    Add import react

--- src/components/profile-view/favoriteMovie.jsx
+++ src/components/profile-view/favoriteMovie.jsx
@@ -1,33 +1,18 @@
-import { useEffect } from 'react';
+import React from 'react';
 import { MovieCard } from '../movie-card/movie-card';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 import PropTypes from 'prop-types';
-import { useSelector } from 'react-redux';
-import { setFavoriteMovies } from '../../redux/user/userSlice';
 
 
-
-export const UserFavoriteMovies = ({ favoriteMovies }) => {
-  const user = useSelector((state) => state.user.user);
-
-  useEffect(() => {
-    const updateOptimisticUI = () => {
-      setFavoriteMovies(user.favoriteMovies);
-    };
-
-    return () => {
-      updateOptimisticUI();
-    };
-  }, [user.favoriteMovies, favoriteMovies]);
-
-
+export const UserFavoriteMovies = ({ user, favoriteMovies }) => {
   return (
     <Row className="favorite-movies d-flex mt-4 h-100">
       {favoriteMovies.map((movie) => (
         <Col md={3} key={movie._id} className='p-3'>
           <MovieCard
             movie={movie}
+            isFavorite={user.favoriteMovies.includes(movie._id)}
           />
         </Col>
       ))}
