5ea51e38f54e98d0d51dce4c766dc0fc18e061bb
Emmanuel Nodolomwanyi <emma0martins@gmail.com>
2024-07-03 14:03:33 +0200

    Add import react

--- src/components/movie-filter/movie-filter.jsx
+++ src/components/movie-filter/movie-filter.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { setFilter } from "../../redux/movies/movieSlice";
 import Form from "react-bootstrap/Form";
