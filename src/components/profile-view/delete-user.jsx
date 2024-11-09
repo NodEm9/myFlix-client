558bdb53a6dcb87ad2dc3515088e5f3ab201911d
Emmanuel Nodolomwanyi <emma0martins@gmail.com>
2024-11-09 23:35:25 +0100

    Clean up codes

--- src/components/profile-view/delete-user.jsx
+++ src/components/profile-view/delete-user.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import './profile-view.scss';
 import Button from "react-bootstrap/Button";
 import ToastNotification from "../toast/toast";
 import { setDeleteUser } from "../../redux/user/userSlice";
@@ -40,7 +41,7 @@ export const DeleteUser = () => {
   };
 
   return (
-    <div>
+    <div className="delete-user">
       <Button
         variant="danger"
         onClick={handleDelete}
