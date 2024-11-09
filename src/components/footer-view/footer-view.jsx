bfd931ed6ff6655854a71ae108276b4930ab4d3c
Emmanuel Nodolomwanyi <emma0martins@gmail.com>
2024-11-09 23:13:24 +0100

    Remove on used markups

--- src/components/footer-view/footer-view.jsx
+++ src/components/footer-view/footer-view.jsx
@@ -1,26 +1,15 @@
 import React from "react";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import Nav from "react-bootstrap/Nav";
 
 export const FooterView = () => {
   return (
-    <Row className="footer bg-dark text-white text-center">
-      <Col className="footer__copyright">
-         &copy; 2024 myFlix. All Rights Reserved.
-      </Col>
-      <Col >   
-        <Nav className="p-2 d-md-flex d-sm-flex flex-column gap-3">
-          <Nav.Link href="/" className="text-primary fs-2 text-decoration-none">
-            myFlix</Nav.Link>
-          <Nav.Link href="/" className="text-white fs-6 text-decoration-none">
-            Home</Nav.Link>
-          <Nav.Link href="/profile" className="text-white fs-6 text-decoration-none">
-            Users</Nav.Link>
-          <Nav.Link href={"https://myflix-app-led6.onrender.com/documentation.html"} target="_blank" className="text-white fs-6 text-decoration-none">
-           API Documentation</Nav.Link>
-        </Nav>
+    <Row md={12} className="footer h-100 bg-dark text-white justify-content-center text-center pt-5 px-5">
+      <Col md={8} className="text-white text-center px-4">
+          <a href="/" className="text-primary fs-2 text-decoration-none">
+            myFlix</a>
+        <div className="text-wrap">Designed &#38; developed by Emmanuel Nodolomwanyi &copy; Copyright 2024.</div>
       </Col>
     </Row>
   );
-};
+}; 
