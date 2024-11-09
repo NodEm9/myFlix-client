c6e6207abf087b7fbe12a3ac573313b55bc33995
Emmanuel Nodolomwanyi <emma0martins@gmail.com>
2024-11-09 23:33:56 +0100

    Clean up codes

--- src/components/profile-view/profile-view.jsx
+++ src/components/profile-view/profile-view.jsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
 import { UpdateUserData } from './updateUser';
 import { DeleteUser } from './delete-user';
 import { UserFavoriteMovies } from './favoriteMovie';
@@ -10,13 +11,14 @@ import './profile-view.scss';
 
 
 export const ProfileView = ({ movies }) => {
+  const [showUpdateForm, setShowUpdateForm] = useState(false);
   const user = JSON.parse(localStorage.getItem('user'));
-  const favoriteMovies = movies.filter((movie) => user.favoriteMovies.includes(movie._id)); 
-
+  const favoriteMovies = movies.filter((movie) =>
+    user.favoriteMovies.includes(movie._id));
 
   return (
-    <Row className="h-100">
-      <Row className='profile-view'>
+    <Row className="h-100 w-100 mt-5">
+      <Row className='profile-view mx-auto'>
         <Col className='profile fs-5'>
           <h2 className='fw-bold'>Account Information</h2>
           <div className="username">
@@ -36,8 +38,18 @@ export const ProfileView = ({ movies }) => {
             <span className='value'>{user.Role}</span>
           </div>
         </Col>
-        <Col className='my-4'>
-          <UpdateUserData user={user} />
+        {/* Update User Data form */}
+        <Col className='h-100'>
+          {showUpdateForm ? (
+            <UpdateUserData user={user} setShowUpdateForm={setShowUpdateForm} />
+          ) : (
+            <Button
+              onClick={() => setShowUpdateForm(true)}
+              className='update-btn w-50 float-end my-4'
+            >
+              Update Account
+            </Button>
+          )}
         </Col>
       </Row>
       <Row className='d-flex justify-content-center'>
@@ -46,10 +58,15 @@ export const ProfileView = ({ movies }) => {
         </Col>
       </Row>
       <hr className='my-4' />
-      <Row className='d-flex justify-content-center'>
-        <h2 className='fw-bold'>Favorite Movies</h2>
-        <Col className='d-flex justify-content-center'>
-          {favoriteMovies && <UserFavoriteMovies user={user} favoriteMovies={favoriteMovies} />}
+      {/* List favorite Movies */}
+      <Row className='justify-content-center'>
+        <h2 className='fw-bold ml-0'>Favorite Movies</h2>
+        <Col className='justify-content-center'>
+          {favoriteMovies.length === 0 ? (
+            <div className='text-center fw-4'>You have no favorite movies.</div>
+          ) : (
+            <UserFavoriteMovies favoriteMovies={favoriteMovies} user={user} />
+          )}
         </Col>
       </Row>
     </Row>
