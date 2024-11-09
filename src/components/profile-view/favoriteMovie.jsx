daf1f98da1575059af0f9e81f42489f33dd2df0a
Emmanuel Nodolomwanyi <emma0martins@gmail.com>
2024-11-09 23:32:49 +0100

    Remove Movie-card component use to render the favorite and  replace with Removie favorite movie component and render both movie tile and the favorite movies in a FavoriteMovie provider

--- src/components/profile-view/favoriteMovie.jsx
+++ src/components/profile-view/favoriteMovie.jsx
@@ -1,19 +1,23 @@
 import React from 'react';
-import { MovieCard } from '../movie-card/movie-card';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 import PropTypes from 'prop-types';
+import FavoriteProvider from '../../context/FavoriteContext';
+import RemoveFromFavorite from './removeFavorite';
 
 
-export const UserFavoriteMovies = ({ user, favoriteMovies }) => {
+export const UserFavoriteMovies = ({ favoriteMovies }) => {
+
   return (
-    <Row className="favorite-movies d-flex mt-4 h-100">
+    <Row className="d-flex flex-column w-100 mt-1 h-100 sm-justify-content-center">
       {favoriteMovies.map((movie) => (
-        <Col md={3} key={movie._id} className='p-3'>
-          <MovieCard
-            movie={movie}
-            isFavorite={user.favoriteMovies.includes(movie._id)}
-          />
+        <Col md={3} key={movie._id} className='p-1'>
+          <FavoriteProvider movie={movie}>
+            <div className='d-flex gap-2 align-items-center'>
+            <RemoveFromFavorite movie={movie} isFavorite={false} />
+            {movie.Title}
+            </div>
+          </FavoriteProvider>
         </Col>
       ))}
     </Row>
